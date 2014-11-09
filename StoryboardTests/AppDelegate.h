c1087c90010346129f0d0f58dc027cb0114a2162
Moritz Grosch (LittleFox) <littlefox@fsfe.org>
2014-11-09 14:31:10 +0100

    Initial Commit

---
+++ StoryboardTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoryboardTests
+//
+//  Created by Moritz Grosch on 09.11.14.
+//  Copyright (c) 2014 LittleFox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoryboardTests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoryboardTests
+//
+//  Created by Moritz Grosch on 09.11.14.
+//  Copyright (c) 2014 LittleFox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
